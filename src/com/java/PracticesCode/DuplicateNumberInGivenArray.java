018643830f00b39c76f1a4ae85457fb0f8cd1630
PriyaRamesh <darshiniramesh1617@outlook.com>
2025-12-02 12:27:47 +0530

    Array Reverse and Duplicate array

---
+++ src/com/java/PracticesCode/DuplicateNumberInGivenArray.java
@@ -0,0 +1,33 @@
+package com.java.PracticesCode;
+
+import java.util.Scanner;
+
+public class DuplicateNumberInGivenArray {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to verify in the array");
+        int num = sc.nextInt();
+        System.out.println("To verify the count in array: "+num);
+        System.out.println("Enter the length of an array");
+        int l =sc.nextInt();
+        int [] a = new int[l];
+        // store the array data or read the array data
+        for(int i =0;i<a.length;i++){
+            a[i] = sc.nextInt();
+        }
+        // check the duplicate array
+        int count =0;
+        boolean status = false;
+        for(int i=0;i<a.length;i++){
+            if(num == a[i]){
+                count += 1;
+                status = true;
+            }
+        }
+        if(status == false){
+            System.out.println("Dont have any duplicate number in the given array");
+        }else {
+            System.out.println("The duplicate number of the array is: "+count);
+        }
+    }
+}
--- src/com/java/PracticesCode/ReverseArray.java
+++ src/com/java/PracticesCode/ReverseArray.java
@@ -1,4 +1,36 @@
 package com.java.PracticesCode;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ReverseArray {
+    public static void main(String[] args) {
+        // reverse using the while
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the length value of an array");
+        int l = sc.nextInt();
+        int  []arr = new int[l];
+        for (int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        // using while loop
+//        int i=0;
+//        int j = arr.length-1;
+//        System.out.println("Original value in array : "+ Arrays.toString(arr));
+//        while (i<=j){
+//            int temp = arr[i];
+//            arr[i] = arr[j];
+//            arr[j] = temp;
+//            i++;
+//            j--;
+//        }
+//        System.out.println("Reverse value in array : "+ Arrays.toString(arr));
+//        sc.close();
+        for(int i = arr.length-1;i>=0;i--){
+            System.out.println(arr[i]);
+        }
+        sc.close();
+    }
+
+
 }
